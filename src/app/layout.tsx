fc8894835dffae1e8f4b50b4af59e485f5eea580
Nisharga <kabir@nexlent.com>
2024-03-06 20:30:45 +0600

    feat: theme toggler

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,37 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css"; 
-import { Footer, Navbar } from "@/components";
-import { ThemeContextProvider } from "@/context/ThemeContextProvider";
-import ThemeProvider from "@/provider/ThemeProvider";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Footer, Navbar } from '@/components';
+import { ThemeContextProvider } from '@/context/ThemeContextProvider';
+import ThemeProvider from '@/provider/ThemeProvider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Blog Nest",
-  description: "Created by Nishsrga Kabir",
+    title: 'Blog Nest',
+    description: 'Created by Nishsrga Kabir'
 };
 
 export default function RootLayout({
-  children,
+    children
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeContextProvider>
-          <ThemeProvider>
-        <div className="container">
-          <div className="wrapper">
-            <Navbar/>
-             {children}
-                           <Footer/>
-            </div>
-          </div>
-          </ThemeProvider>
-          </ThemeContextProvider>
-        </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={inter.className}>
+                <ThemeContextProvider>
+                    <ThemeProvider>
+                        <div className='container'>
+                            <div className='wrapper'>
+                                <Navbar />
+                                {children}
+                                <Footer />
+                            </div>
+                        </div>
+                    </ThemeProvider>
+                </ThemeContextProvider>
+            </body>
+        </html>
+    );
 }
