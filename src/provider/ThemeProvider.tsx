0a9400ffbc5ef261602ddd05855df50317b933a8
Nisharga <kabir@nexlent.com>
2024-03-07 00:11:53 +0600

    fix: header issue

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { ThemeContext } from '@/context/ThemeContextProvider';
-import React, { useContext, FC } from 'react';
+import React, { useContext, FC, useEffect, useState } from 'react';
 
 interface IProps {
     children: React.ReactNode;
@@ -8,7 +8,14 @@ interface IProps {
 
 const ThemeProvider: FC<IProps> = ({ children }) => {
     const { theme } = useContext(ThemeContext);
-    return <div className={theme}>{children}</div>;
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (mounted) {
+        return <div className={theme}>{children}</div>;
+    }
 };
 
 export default ThemeProvider;
