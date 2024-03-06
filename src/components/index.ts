fc037b0f77ad7ba896c96efce8bcb7fa39daa024
Nisharga <kabir@nexlent.com>
2024-03-06 10:04:36 +0600

    setup all components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from "./CardList";
+export * from "./CategoryList";
+export * from "./Featured";
+export * from "./Footer";
+export * from "./Menu";
+export * from "./Navbar";
+export * from "./Pagination";
