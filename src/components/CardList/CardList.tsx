25dfe75689c2ea9756e85b49da0d2a8ea3b2928b
Nisharga <kabir@nexlent.com>
2024-03-07 00:31:04 +0600

    fix: header issue

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Pagination } from '..'
+import React from 'react';
+import { Pagination } from '..';
+import styles from './CardList.module.css';
 
 const CardList = () => {
-  return (
-    <div>
-        <Pagination/>
-    </div>
-  )
-}
+    return (
+        <div>
+            <div className={styles.container}>
+                <h1 className={styles.title}>Recent Posts</h1>
+                <div className={styles.posts}>
+                    {/* {posts?.map((item) => (
+          <Card item={item} key={item._id} />
+        ))} */}
+                </div>
+                <Pagination />
+            </div>
+        </div>
+    );
+};
 
-export default CardList
+export default CardList;
--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import styles from './CategoryList.module.css';
 
 const CategoryList = () => {
-  return (
-    <div>CategoryList</div>
-  )
-}
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Popular Categories</h1>
+            <div className={styles.categories}>
+                {/* {data?.map((item) => (
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles[item.slug]}`}
+          key={item._id}
+        >
+          {item.img && (
+            <Image
+              src={item.img}
+              alt=""
+              width={32}
+              height={32}
+              className={styles.image}
+            />
+          )}
+          {item.title}
+        </Link>
+      ))} */}
+            </div>
+        </div>
+    );
+};
 
-export default CategoryList
+export default CategoryList;
