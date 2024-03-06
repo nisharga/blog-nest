8f738d8e137c5ccc27fa7e76fba492d325cc7732
Nisharga <kabir@nexlent.com>
2024-03-06 15:35:07 +0600

    format code

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -1,18 +1,15 @@
-"use client"
-import { ThemeContext } from '@/context/ThemeContextProvider'
-import React, { useContext, FC } from 'react'
+"use client";
+import { ThemeContext } from "@/context/ThemeContextProvider";
+import React, { useContext, FC } from "react";
 
-interface IProps{
-    children: React.ReactNode
+interface IProps {
+  children: React.ReactNode;
 }
 
-const ThemeProvider:FC<IProps> = ({children}) => {
-    const { theme } = useContext(ThemeContext);
-  return (
-    <div className={theme}>
-        {children}
-    </div>
-  )
-}
+const ThemeProvider: FC<IProps> = ({ children }) => {
+  const { theme } = useContext(ThemeContext);
 
-export default ThemeProvider
+  return <div className={theme}>{children}</div>;
+};
+
+export default ThemeProvider;
