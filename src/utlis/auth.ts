8252f99dafaf953956e5996ad1b149d99228641b
Nisharga <kabir@nexlent.com>
2024-03-15 23:23:27 +0600

    feat: prisma get method added

--- src/utlis/auth.ts
+++ src/utlis/auth.ts
@@ -1,7 +1,11 @@
 // import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from './connect';
+import { getServerSession } from 'next-auth';
 
 export const authOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         /*  GithubProvider({
           clientId: process.env.GITHUB_ID,
@@ -14,3 +18,5 @@ export const authOptions = {
         // ...add more providers here
     ]
 };
+
+export const getAuthSession = () => getServerSession(authOptions);
